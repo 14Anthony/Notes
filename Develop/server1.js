77708fea2f6669e539a9f1d1ec9afde9f35c0f73
Anthony Belcastro <belcastt@live.com>
2020-08-17 13:30:39 -0700

    create a get request route to display the JSON of all the notes.

--- Develop/server1.js
+++ Develop/server1.js
@@ -9,3 +9,20 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Routes
+// =============================================================
+// Basic route that sends the user first to the AJAX Page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+// Displays all notes
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data);
+        return res.json(JSON.parse(data));
+    });
+});
